8921d9357f4714939bab91d76b7d6b8955159e06
nsnkvs_08 <Sofiya.novikova.2019@mail.ru>
2026-01-22 18:57:32 +0300

    PZ_8

---
+++ PZ_8/PZ_8_1.py
@@ -0,0 +1,10 @@
+#Переименуйте 'city' в 'location'.
+
+sample_dict = {
+ "name": "Kelly",
+ "age": 25,
+ "salary": 8000,
+ "city": "New york"
+}
+sample_dict["location"] = sample_dict.pop("city") 
+print(sample_dict)
