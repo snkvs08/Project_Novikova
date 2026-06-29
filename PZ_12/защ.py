7671d9ab115d625b9a63a02aaa2e4af9fda519fc
nsnkvs_08 <Sofiya.novikova.2019@mail.ru>
2026-06-29 18:20:49 +0300

    PZ

--- PZ_12/защ.py
+++ PZ_12/защ.py
@@ -1,6 +1,7 @@
 import random
 A = [random.randint(-100, 100) for i in range(10)]
 B = [i for i in A if (i < 0) and (i % 5 == 0)]
+
 print("Список А:", A)
 print("Список B:", B)
 print("Количество чисел списка B:", len(B))
